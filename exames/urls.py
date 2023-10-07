7e3296f84699bd24b863fb151effa2a622c0e0cc
Adryan1928 <adryanoliveira1928@gmail.com>
2023-10-07 14:33:45 -0300

    Gerenciar pedidos concluído

--- exames/urls.py
+++ exames/urls.py
@@ -5,5 +5,7 @@ app_name = 'exames'
 
 urlpatterns = [
     path('solicitar_exames/', view=views.solicitar_exames, name='solicitar_exames'),
-    path('fechar_pedido/', view=views.fechar_pedido, name='fechar_pedido')
+    path('fechar_pedido/', view=views.fechar_pedido, name='fechar_pedido'),
+    path('gerenciar_pedidos/', view=views.gerenciar_pedidos, name="gerenciar_pedidos"),
+    path("cancelar_pedido/<int:pedido_id>", view=views.cancelar_pedido, name="cancelar_pedido"),
 ]
